ad305fe6b82a14ec60b36953eb1628c4acc98cdb
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2020-03-28 18:21:04 -0300

    init do onesignal

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
+import '../environments/environments.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    OneSignal.shared.init(oneSignalAppID);
+
     return MaterialApp(
       title: 'Flutter Slidy',
       theme: ThemeData(
