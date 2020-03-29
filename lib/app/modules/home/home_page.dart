d0cf0c030b0b08911068c61a96e30ab646924a98
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2020-03-29 11:18:06 -0300

    tratando a mensagem quando o aplicativo tiver aberto

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 
 class HomePage extends StatefulWidget {
   final String title;
@@ -9,14 +10,64 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  showAlertDialog(BuildContext context, String title, String message) {
+    // set up the button
+    Widget okButton = FlatButton(
+      child: Text("OK"),
+      onPressed: () {},
+    );
+
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      title: Text(title),
+      content: Text(message),
+      actions: [
+        okButton,
+      ],
+    );
+
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    OneSignal.shared.setInFocusDisplayType(OSNotificationDisplayType.none);
+
+    OneSignal.shared.setNotificationReceivedHandler((displayType) {
+      for (var item in displayType.payload.additionalData.entries) {
+        print(item);
+      }
+      showAlertDialog(
+          context, displayType.payload.title, displayType.payload.body);
+      print('Clicou setNotificationReceivedHandler');
+    });
+
+    OneSignal.shared.setNotificationOpenedHandler((openedResult) {
+      print('Clicou setNotificationOpenedHandler');
+    });
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
       body: Column(
-        children: <Widget>[],
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: RaisedButton(
+              child: Text('Show alert'),
+              onPressed: () {
+                showAlertDialog(context, "Titulo", "Mensagem");
+              },
+            ),
+          )
+        ],
       ),
     );
   }
